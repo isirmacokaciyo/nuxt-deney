f700c8aa4642716797620104d3e50dd8a78c72da
isirmacokaciyo <94755848+isirmacokaciyo@users.noreply.github.com>
2021-12-09 21:18:11 +0300

    Add files via upload

---
+++ components/Tutorial.vue
@@ -0,0 +1,14 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 bg-gray-900"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <h1 class="text-white font-black text-5xl">
+      Made With <a href="https://nuxtjs.org">NuxtJS</a>!
+    </h1>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      likes: 0,
+    }
+  }
+};
+</script>
