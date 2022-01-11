1890f78dd6958bbe51a53259eb516c8865d07bd0
isirmacokaciyo <94755848+isirmacokaciyo@users.noreply.github.com>
2022-01-11 17:20:32 +0300

    Update Layer.vue

--- components/Layer.vue
+++ components/Layer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="grid place-items-center h-screen bg-indigo-200">
     <h1 class="text-4xl font-bold text-white">
-      I am rendered on the <strong class="bg-clip-text text-transparent bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 underline decoration-indigo-500">{{ renderedOn }}</strong> side
+      I am rendered on the <strong class="animate-pulse bg-clip-text text-transparent bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 underline decoration-indigo-500">{{ renderedOn }}</strong> side
     </h1>
   </div>
 </template>
